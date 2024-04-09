d76ec4613fd4847596deca06a03de9cec1157954
codeswithrohit <hrohit320@gmail.com>
2024-04-09 07:35:59 +0530

    remove changes

--- pages/index.js
+++ pages/index.js
@@ -368,7 +368,7 @@ const Index = () => {
       {loadingUserData ? (
       // Render spinner here
       <div class='flex space-x-2 justify-center items-center bg-white h-screen dark:invert'>
-      <span class='sr-only'>Loadin...</span>
+      <span class='sr-only'>Loading...</span>
        <div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]'></div>
      <div class='h-8 w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]'></div>
      <div class='h-8 w-8 bg-black rounded-full animate-bounce'></div>
